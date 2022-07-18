85eaede53c3cdff21c38becaafa50340b054f499
Sakhi <sakhybig@gmail.com>
2022-07-18 02:25:51 +0200

    Addded feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id==request.user.id#checks if user attempting to edit is the correct user
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """allow user to update their own status"""
+
+    def has_object_permission(self,request,view,obj):
+        """check if the user is editing their own status"""
+        if(request.method in permissions.SAFE_METHODS):
+            return True
+
+        return obj.user_profile.id==request.user.id
